a311187332eaac60704be341e72cb84b8f987fae
Mikenziokah <mikenziokah@gmail.com>
2024-04-02 20:57:56 +0000

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,4 +18,20 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """adding items in the cache
         """
+        if key i None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded_key = self.queue.pop(0)
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve an item from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
 
